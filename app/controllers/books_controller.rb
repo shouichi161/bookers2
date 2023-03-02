4a70ade2523fffac944ca71673daec3301995556
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-02 06:36:32 +0000

    [update]books.index.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
   belongs_to:user
   has_one_attached:image
-  has_one_attached:profile_image
+  
+  
+  def get_image
+    
+  end
 
  
 end
