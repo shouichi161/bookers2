e092fe3cbb3a07702471fba1cfc3d96f20621c93
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-03 09:13:28 +0000

    [update]user_show/edit,update,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,12 +13,26 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book=Book.find(params[:id])
+    @book=Book.new
+    @bookf=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
-
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
 
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
-
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
 
   def index
+    @book=Book.new
     @users=User.all
   end
 
   def show
+    @book=Book.new
     @user=User.find(params[:id])
+    @books=@user.books
+    
+    
 
   end
 
@@ -15,12 +25,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user=User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    user=User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
  private
+
+ def book_params
+   params.require(:book).permit(:title,:body)
+ end
 
  def user_params
    params.require(:user).permit(:name,:profile_image,:introduction)
